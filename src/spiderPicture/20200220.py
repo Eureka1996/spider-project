b715ddb177b18114e1a263c81acac0a3078c8b23
Eureka1996 <wufuqianglin@163.com>
2020-02-26 15:07:15 +0800

    增加线程池

--- src/spiderPicture/20200220.py
+++ src/spiderPicture/20200220.py
@@ -1,85 +1,149 @@
 # -*- coding: utf-8 -*-
 
-import socket
 import urllib.request
 import urllib.error
 import urllib.parse
 from lxml import etree
 from concurrent.futures import ThreadPoolExecutor
 import os
+import string
 
+# pictureUrlExecutor = ThreadPoolExecutor(max_workers=5)
+loadExecutor = ThreadPoolExecutor(max_workers=25)
+localPath = 'D:\\MyDrivers\\update\\photo'
 
-def loadPicture(url,localPath,filename):
-    filenameTmp = localPath+'/'+filename
+def loadPicture(url,localPath,filename,pictureIndex,reLoadTime):
+    if reLoadTime > 5:
+        return
+    filenameTmp = '%s\\%s' % (localPath,filename)
     try:
-        if os.path.exists(filenameTmp):
-            print(filenameTmp+"已经存在")
-        else:
-            print('保存图片：' + url+"->"+filenameTmp)
-            pictureContent = urllib.request.urlopen(url,timeout=20).read()
+        if not os.path.exists(filenameTmp):
+            print('\t\t(%d,%d,%d)下载图片%d：%s,%s'%(pictureIndex[0],pictureIndex[1],pictureIndex[2],reLoadTime,filename,url))
+            pictureContent = urllib.request.urlopen(url,timeout=120).read()
             with open(filenameTmp, "wb") as f:
                 f.write(pictureContent)
-            # urllib.request.urlretrieve(url, filename=localPath + '/' + filename)
+            print('\t\t(%d,%d,%d)下载图片完成：%s' % (pictureIndex[0], pictureIndex[1],pictureIndex[2], filename))
+        else:
+            print('\t\t(%d,%d,%d)图片已经存在：%s' % (pictureIndex[0], pictureIndex[1],pictureIndex[2], filename))
     except Exception as e:
-        loadPicture(url,localPath,filename)
+        loadPicture(url, localPath, filename, pictureIndex, reLoadTime+1)
 
 
-def crawPictureUrl(url):
-    html = urllib.request.urlopen(url).read()
-    html = etree.HTML(html)
+def getFilename(pictureUrl,pictureName):
+    numIndex = str(pictureUrl).rfind('/')
+    extendTmp = pictureUrl[numIndex+1:]
+    filename = '%s%s' % (pictureName,extendTmp)
+    return filename
 
+# 获取图片集的信息：名称，图片数量
+def getPictureSetDesc(url):
     divCount = 2
-    pictureNames = html.xpath('//div[@class="main"]/div['+str(divCount)+']/a/@title')
-    if len(pictureNames) == 0 :
-        divCount = 3
-        pictureNames = html.xpath('//div[@class="main"]/div[' + str(divCount) + ']/a/@title')
+    parseUrlResult = parseUrl(url,'//div[@class="main"]/div[' + str(divCount) + ']/a/@title')
+    # 获取图片名称
+    pictureSetNames = parseUrlResult[1]
+    while len(pictureSetNames) == 0:
+        divCount += 1
+        pictureSetNames = parseUrlResult[0].xpath('//div[@class="main"]/div[' + str(divCount) + ']/a/@title')
+    pictureSetName = pictureSetNames[0]
 
-    pictureName = pictureNames[0]
-    pictureUrls = html.xpath('//div[@class="main"]/div['+str(divCount)+']/a/img/@src')
-
-    for pictureUrl in pictureUrls:
-        print("pictureUrl:"+pictureUrl)
-        filename = str(pictureName) + pictureUrl[str(pictureUrl).rfind('/')+1:]
-        loadPicture(pictureUrl, './photo', filename)
-    pageCount = divCount + 1
-    pageColumn = html.xpath('//div[@class="main"]/div['+str(pageCount)+']/a[last()-1]')
-
-    if len(pageColumn) == 0:
-        pageCount+=1
-        pageColumn = html.xpath('//div[@class="main"]/div['+str(pageCount)+']/a[last()-1]')
-
+    # 获取图片页数
+    pageDivCount = divCount + 1
+    pageColumn = parseUrlResult[0].xpath('//div[@class="main"]/div[' + str(pageDivCount) + ']/a[last()-1]')
+    while len(pageColumn) == 0:
+        pageDivCount += 1
+        pageColumn = parseUrlResult[0].xpath('//div[@class="main"]/div[' + str(pageDivCount) + ']/a[last()-1]')
     pictureCount = pageColumn[0].text
+    return (pictureSetName,pictureCount)
+
+def load(url,pictureSetDesc,pictureIndex):
+    pictureDivCount = 2
+    parseUrlResult = parseUrl(url,'//div[@class="main"]/div['+str(pictureDivCount)+']/a/img/@src')
+    pictureSrcs = parseUrlResult[1]
+    while len(pictureSrcs) == 0:
+        pictureDivCount+=1
+        pictureSrcs = parseUrlResult[0].xpath('//div[@class="main"]/div[' + str(pictureDivCount) + ']/a/img/@src')
+
+    for pictureSrc in pictureSrcs:
+        filename = getFilename(pictureSrc, pictureSetDesc[0])
+        pictureCount = pictureSetDesc[1]
+        loadPicture(pictureSrc,localPath,filename,(pictureIndex[0],pictureIndex[1],int(pictureCount)),0)
+
+
+
+def crawPictureUrl(url,pictureIndex):
+
+    pictureSetDesc = getPictureSetDesc(url)
+
     preUrl = url[:url.rfind('.')]
-    for i in range(2,int(pictureCount)+1):
-        newUrl = preUrl+"_"+str(i)+".html"
-        print(pictureCount+','+newUrl)
-        newHtml = urllib.request.urlopen(newUrl).read()
-        newHtml = etree.HTML(newHtml)
-        pictureUrls = newHtml.xpath('//div[@class="main"]/div['+str(divCount)+']/a/img/@src')
-        for pictureUrl in pictureUrls:
-            filename = str(pictureName) + pictureUrl[str(pictureUrl).rfind('/') + 1:]
-            loadPicture(pictureUrl, './photo', filename)
+    for i in range(1,int(pictureSetDesc[1])+1):
+        pictureUrl = url
+        if i != 1:
+            pictureUrl = preUrl + "_" + str(i) + ".html"
+        # print('pictureUrl:%s'%pictureUrl)
+        loadExecutor.submit(load,pictureUrl,pictureSetDesc,pictureIndex)
+        # load(pictureUrl,pictureSetDesc,pictureIndex)
 
-    return pictureName,pictureUrls
+def containTag(url,tag):
+    result = False
+    elementTags = parseUrl(url,'//div[@class="main"]/div[1]/div[1]/div[2]/div[3]/p[2]/a')[1]
+    for elementTag in elementTags:
+        if elementTag.text is None:
+            continue
+        result = elementTag.text.find(tag) != -1
+        if result:
+            break
+    return result
 
-def crawIndexPage(url):
-    indexPageContent = urllib.request.urlopen(url).read()
-    indexPageHtml = etree.HTML(indexPageContent)
-    indexPagePictureUrls = indexPageHtml.xpath('//div[@class="index_list"]/div[@class="index_pic"]/ul/li/a/@href')
-    return indexPagePictureUrls
 
-if __name__ == '__main__':
+def parseUrl(url,tag):
+    result = []
+    html = None
+    while html is None:
+        try:
+            html = urllib.request.urlopen(url,timeout=60).read()
+            html = etree.HTML(html)
+            result = html.xpath(tag)
+        except Exception as e:
+            pass
+            # print('parseUrl error:%s,%s'%(str(type(e)),url))
+    return (html,result)
+
+
+
+def main():
     preUrl = 'http://www.9900rt.org/html/yazhou/'
     website = 'http://www.9900rt.org/'
-    executor = ThreadPoolExecutor(max_workers=20)
-    for i in range(1,188):
+
+    for i in range(40,188):
         url = ''
         if i == 1:
             url = preUrl
         else:
             url = preUrl + str(i)+'.html'
 
-        indexPagePictureUrls = crawIndexPage(url)
+        indexPagePictureUrls = []
+        while len(indexPagePictureUrls) == 0:
+            indexPagePictureUrls = parseUrl(url,'//div[@class="index_list"]/div[@class="index_pic"]/ul/li/a/@href')[1]
+        count = 0
         for indexPagePictureUrl in indexPagePictureUrls:
+            count+=1;
+            print("(%d,%d)"%(i,count))
             indexPagePictureUrl = website + indexPagePictureUrl
-            executor.submit(crawPictureUrl,indexPagePictureUrl)
+            # 判断是否包含指定的标签
+            isContainTag = containTag(indexPagePictureUrl,'日本')
+            if not isContainTag:
+                print('\t(%d,%d)爬取图片链接：%s'%(i,count,indexPagePictureUrl))
+                crawPictureUrl(indexPagePictureUrl,(i,count))
+
+def main2():
+    urls = [
+        'http://www.9900rt.info/html/yazhou/2016/0805/4355.html',
+        'http://www.9900rt.info/html/yazhou/2017/0921/5913.html',
+        'http://www.9900rt.info/html/yazhou/2017/1015/5997.html'
+    ]
+
+    for url in urls:
+        crawPictureUrl(url, (0, 0))
+
+if __name__ == '__main__':
+    main()
